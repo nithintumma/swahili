c7eac4339cf80e1d5f9dc10b1be4dab80849aa28
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2015-04-30 12:54:09 -0400

    Render view function

--- swahili/lessons/views.py
+++ swahili/lessons/views.py
@@ -258,7 +258,7 @@ def lesson_home(request):
     data['sentence_text'] = sentence_to_text(rand_sentence)
     data['all_data'] = gen_all_sentences()
     #return HttpResponse(json.dumps(data), content_type='application/json')
-    return render("swahili/home.html", data)
+    return render(request, "swahili/home.html", data)
 
 def lesson_change(request):
     """
